b933edd616c90963b4f13963a198f704be8a5dca
YunmingYan <47409370+YunmingYan@users.noreply.github.com>
2019-08-17 17:42:23 +0800

    Add files via upload

--- js/address.js
+++ js/address.js
@@ -1,5 +1,6 @@
 //var A = "http://localhost:8080";
 //var A = "193.112.11.189:8888";
 //var A = "192.168.31.8:8888";//局域网
-var A = "111.230.89.193:8888";//外网
+//var A = "111.230.89.193:8888";//外网
 //var videoAddress = "view/player/sample/rtmp.html";
+var A = "192.168.1.118:8080"//大创
